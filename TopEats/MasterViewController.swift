b6cbacc4a41bcfb99d98e51a4e2cbf9a1681979e
Mbusi Hlatshwayo <mbusi@cox.net>
2017-06-11 15:31:36 -0700

    Embedded views in navigation controller to allow back swipe. Need to hide nav bar

--- TopEats/MasterViewController.swift
+++ TopEats/MasterViewController.swift
@@ -21,6 +21,20 @@ class MasterViewController: UIViewController {
         setupView()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Hide the navigation bar on the this view controller
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Show the navigation bar on other view controllers
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
     func setupView() {
         // do view stuff here
         segmentedControl.addTarget(self, action: #selector(selectionDidChange(_:)), for: .valueChanged)
